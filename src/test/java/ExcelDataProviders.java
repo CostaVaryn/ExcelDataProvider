11de5ad7785cbdcefd102c3e7120d1bd72957769
Costa Varyn <d3tug@yandex.ru>
2022-08-15 17:21:54 +0300

    add ExcelDataProviders.java

---
+++ src/test/java/ExcelDataProviders.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.DataProvider;
+
+
+public class ExcelDataProviders {
+
+    @DataProvider
+    public Object[][] usersFromSheet1() throws Exception {
+        String path = "src/main/resources/users.xlsx";
+        ExcelReader excelReader = new ExcelReader(path);
+        return excelReader.getSheetDataForTDD();
+    }
+
+    @DataProvider
+    public Object[][] usersFromSheet2() throws Exception {
+        String path = "src/main/resources/users.xlsx";
+        ExcelReader excelReader = new ExcelReader(path,"Лист2");
+        return excelReader.getCustomSheetForTDD();
+    }
+}
