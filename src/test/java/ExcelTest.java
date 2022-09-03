496233ce5d6e5b48e69e08b3eb664464fa837821
Constantin <53481895+CostaVaryn@users.noreply.github.com>
2022-09-03 17:13:27 +0300

    Update ExcelTest.java

--- src/test/java/ExcelTest.java
+++ src/test/java/ExcelTest.java
@@ -5,24 +5,23 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 import static io.restassured.RestAssured.given;
 
 public class ExcelTest {
 
     @Test(dataProvider = "usersFromSheet1", dataProviderClass = ExcelDataProviders.class)
-    public void test(String... params){
+    public void test(String... params) {
         System.out.println("User " + params[1] + " has ID number " + params[0] + " surname " + params[2]);
     }
 
     @Test(dataProvider = "usersFromSheet2", dataProviderClass = ExcelDataProviders.class)
-    public void test2(String params1, String params2){
+    public void test2(String params1, String params2) {
         System.out.println("Login: " + params1);
         System.out.println("Password: " + params2);
     }
 
     @Test(dataProvider = "usersFromApi", dataProviderClass = ExcelDataProviders.class)
-    public void checkUsers(String... params){
+    public void checkUsers(String... params) {
         int id = (int) Double.parseDouble(params[0]);
         Response response = given().
                 contentType(ContentType.JSON)
@@ -36,7 +35,6 @@ public class ExcelTest {
         Assert.assertEquals(jsonPath.getString("data.last_name"), params[3]);
         Assert.assertEquals(jsonPath.getString("data.avatar"), params[4]);
     }
-
     /**
      * @DataProvider
      * public Object[][] data(){
